55cb002f09782f91ab1182910b9cafdda170a17a
JWFJTQ <a@ip404.com>
2018-04-18 20:54:52 +0800

    Update queue.py

--- Code/data_structures/queue/queue.py
+++ Code/data_structures/queue/queue.py
@@ -7,5 +7,21 @@ class AbstractQueue(abc.ABC):
     pass
 
 
+class LinkedQueue(AbstractQueue):
+    def __init(self, name):
+        pass
+
+    def enqueue(self):
+        pass
+    
+    def dequeue(self):
+        pass
+    
+    def length(self):
+        pass
+    
+    def is_empty(self):
+        pass
+
 if __name__ == "__main__":
     print('queue main')
